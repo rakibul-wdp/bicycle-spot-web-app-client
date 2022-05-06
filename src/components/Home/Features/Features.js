1279da3005057c82532eb227d2e2282825a874c8
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-07 02:06:46 +0600

    add features section

---
+++ src/components/Home/Features/Features.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import './Features.css';
+
+const Features = () => {
+  return (
+    <div className='container'>
+      <div className='text-center'>
+        <h2>
+          OUR <span>FEATURES</span>
+        </h2>
+        <hr className='w-25 mx-auto features-horizontal' />
+        <p>We will provide multiple features for service. Like air freight, time bound delivery etc.</p>
+      </div>
+      <div className='features-items'>
+        <div className='features-item'>
+          <img src='https://i.ibb.co/dkJJ4wf/features-1.png' alt='' />
+          <div>
+            <h4>ECONOMICAL AIR FREIGHT</h4>
+            <hr />
+            <p>
+              Canvas metri essar. Incubator ramen viral product management drect mailing. such founders gamification
+              Effct.
+            </p>
+          </div>
+        </div>
+        <div className='features-item'>
+          <img src='https://i.ibb.co/q1fCNvn/features-2.png' alt='' />
+          <div>
+            <h4>WAREHOUSING</h4>
+            <hr />
+            <p>
+              Canvas metri essar. Incubator ramen viral product management drect mailing. such founders gamification
+              Effct.
+            </p>
+          </div>
+        </div>
+        <div className='features-item'>
+          <img src='https://i.ibb.co/Srmnjyd/features-3.png' alt='' />
+          <div>
+            <h4>SAME DAY DELIVERY</h4>
+            <hr />
+            <p>
+              Canvas metri essar. Incubator ramen viral product management drect mailing. such founders gamification
+              Effct.
+            </p>
+          </div>
+        </div>
+        <div className='features-item'>
+          <img src='https://i.ibb.co/HrYbQhp/features-4.png' alt='' />
+          <div>
+            <h4>PACKAGING AND STORAGE</h4>
+            <hr />
+            <p>
+              Canvas metri essar. Incubator ramen viral product management drect mailing. such founders gamification
+              Effct.
+            </p>
+          </div>
+        </div>
+        <div className='features-item'>
+          <img src='https://i.ibb.co/58jn10t/features-5.png' alt='' />
+          <div>
+            <h4>TIME BOUND DELIVERIES</h4>
+            <hr />
+            <p>
+              Canvas metri essar. Incubator ramen viral product management drect mailing. such founders gamification
+              Effct.
+            </p>
+          </div>
+        </div>
+        <div className='features-item'>
+          <img src='https://i.ibb.co/T8n6s7h/features-6.png' alt='' />
+          <div>
+            <h4>MULTI-MODAL TRANSPORT</h4>
+            <hr />
+            <p>
+              Canvas metri essar. Incubator ramen viral product management drect mailing. such founders gamification
+              Effct.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Features from '../Features/Features';
+import Inventory from '../Inventory/Inventory';
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Features />
+      <Inventory />
     </div>
   );
 };
